73517c9d7167ba716e1945967ec525698d098ee5
Ahmed Safwat Tawfik <asafwat@ejadaalex.local>
2018-06-28 17:58:01 +0200

    mock User interface and data

---
+++ src/mocks/user.mocks.ts
@@ -0,0 +1,23 @@
+import { User } from "../models/user.interface";
+
+
+const userList:User[]=[
+    {
+        name:'PaulHalliday',
+        company:'PWH',
+        location:'Durham, UK',
+        bio:'Imake videos on my favourite technologies, Go Angular!',
+        avatar_url:'http://i.imgur.com/jav62p6.jpg',
+        email:'paul@paul.com'
+    },
+    {
+        name:'JohnDoe',
+        company:'Doe and Co.',
+        location:'London, UK',
+        bio:'I love Ionic',
+        avatar_url:'http://i.imgur.com/TzWcihb.pnng',
+        email:'john@doe.com'
+    }
+]
+
+export const USER_LIST=userList;
